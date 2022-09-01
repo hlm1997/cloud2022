aee0cf23c8c51e34421bb660c04e29e5f66d42c3
Helm <Helm@qq.com>
2022-09-01 09:55:54 +0800

    config客户端配置与测试\config客户端之动态刷新

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author Helimin
+ * @Date 2022/9/1 9:47
+ * @Description: TODO
+ * @Version 1.0
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+
+}
