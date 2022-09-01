49cee98a31bfd7e044d25cee12005ea94d8122f2
Helm <Helm@qq.com>
2022-09-01 09:33:04 +0800

    SpringCloud Config 分布式配置中心

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author Helimin
+ * @Date 2022/8/31 23:16
+ * @Description: TODO
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
