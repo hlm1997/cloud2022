30007b8579a660ddc4c20fdbd6b0218f7ea18d1e
Helm <Helm@qq.com>
2022-09-01 15:32:45 +0800

    SpringCloud BUS消息总线

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author Helimin
+ * @Date 2022/9/1 14:56
+ * @Description: TODO
+ * @Version 1.0
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
